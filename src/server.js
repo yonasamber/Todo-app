208eb6c9ddb658ad3eda294ad8df04c510dd8a4a
Yonas <yonasamber@gmail.com>
2025-12-19 11:52:39 +0300

    added sending a file response

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import express from "express";
-
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const PORT = process.env.PORT || 1616;
-console.log("hithere");
 
-app.get("/", (req, res) => {});
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "inde.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
