baf6ced1d545293e7f4d3e0ca8c82bdb60a5fb88
Yonas <yonasamber@gmail.com>
2025-12-19 11:06:28 +0300

    add css and html to the public dir

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 1616;
+console.log("hithere");
+
+app.get("/", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
